a7bcc5665e2051ffa57378cbd71a2da2791de447
Daniel Annankra <dannankra@gmail.com>
2026-03-03 09:23:50 -0500

    fix: revert to server uploads since sharp is now properly configured and client uploads trigger CORS

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -43,7 +43,6 @@ export default buildConfig({
                 media: true
             },
             token: process.env.BLOB_READ_WRITE_TOKEN!,
-            clientUploads: true,
         }),
     ],
     sharp,
