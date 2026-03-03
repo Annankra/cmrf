3ae4a327fb5f1aff2fb83bc92dd8c63e31bde65e
Daniel Annankra <dannankra@gmail.com>
2026-03-03 09:16:14 -0500

    fix: disable local storage and re-enable clientUploads for Vercel Blob

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -43,6 +43,7 @@ export default buildConfig({
                 media: true
             },
             token: process.env.BLOB_READ_WRITE_TOKEN!,
+            clientUploads: true,
         }),
     ],
     sharp,
