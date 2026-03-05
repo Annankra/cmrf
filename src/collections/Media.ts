7f8ba8109900333bc3fb8b9b7bc0d6263ad855bc
Daniel Annankra <dannankra@gmail.com>
2026-03-05 12:03:33 -0500

    fix: make alt field optional to allow URL-based and bulk media uploads

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -34,7 +34,7 @@ export const Media: CollectionConfig = {
         {
             name: 'alt',
             type: 'text',
-            required: true,
+            required: false,
         },
     ],
 }
